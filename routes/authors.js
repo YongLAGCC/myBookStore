5868a1598c864f9741e0e9aaa6b0ca6efc09e2a9
zhou yong <yong@zhous-MacBook-Pro.local>
2020-08-09 01:18:42 -0400

    new users

--- routes/authors.js
+++ routes/authors.js
@@ -1,34 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const author = require("../models/author");
 
 //all authors router
-router.get("/", (req, res) => {
-  res.render("authors/index");
+router.get("/", async (req, res) => {
+  try {
+    const authors = await Author.find({});
+    res.render("authors/index", { authors: authors });
+  } catch {
+    res.redirect("/");
+  }
 });
 
 //new author router
 router.get("/new", (req, res) => {
-  res.render("authors/new", { author: Author() });
+  res.render("authors/new", { author: new Author() });
 });
 //create author router
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
     // 确定传的参是 name， 不是id 或其他
   });
-  author.save((err, newAuthor) => {
-    if (err) {
-      res.render("authors/new", {
-        author: author,
-        errorMes: "Error creating Author",
-      });
-    } else {
-      //res.redirect(`author/${newAuthor.id}`)
-      res.redirect(`authors`);
-    }
-  });
-  // res.send(req.body.name);
+  const locals = { errorMessage: "something went wrong" };
+  try {
+    const createAuthor = await author.save();
+    //res.redirect(`authors/${newAuthor.id}`)
+    res.render(`authors`, locals);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author",
+    });
+  }
 });
+// author.save((err, newAuthor) => {
+//   if (err) {
+//     res.render("authors/new", {
+//       author: author,
+//       errorMessage: "Error creating Author",
+//     });
+//   } else {
+//     //res.redirect(`authors/${newAuthor.id}`)
+//     res.redirect(`authors`, locals);
+//   }
+
+// res.send(req.body.name);
+// });
 
 module.exports = router;
