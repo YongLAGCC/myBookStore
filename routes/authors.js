0cd43b5194584d2b81b2af2bf1f024607647d14e
zhou yong <yong@zhous-MacBook-Pro.local>
2020-08-22 00:54:35 -0400

    authors_routes

--- routes/authors.js
+++ routes/authors.js
@@ -4,28 +4,40 @@ const Author = require("../models/author");
 
 //all authors router
 router.get("/", async (req, res) => {
+  const searchOptions = {};
+  if (req.query.name !== null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+    // i is case insensitive
+  }
   try {
-    const authors = await Author.find({});
-    res.render("authors/index", { authors: authors });
+    // display and find one user from mongodb
+    const authors = await Author.find(searchOptions); // empty means all authors, then pass into the index page by
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+      // send back to user in input value
+    });
   } catch {
+    // if something wrong, we go to home page.
     res.redirect("/");
   }
 });
 
 //new author router
 router.get("/new", (req, res) => {
-  res.render("authors/new", { author: new Author() });
+  res.render("authors/new", { author: new Author() }); // why to give obj for ejs to use
 });
+
 //create author router
 router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
     // 确定传的参是 name， 不是id 或其他
   });
-
   const locals = { errorMessage: "something went wrong" };
   try {
     const createAuthor = await author.save();
+    // mongoose is async, wait till save complete, then poplute the createAuthor variable
     //res.redirect(`authors/${newAuthor.id}`)
     res.render(`authors`, locals);
   } catch {
